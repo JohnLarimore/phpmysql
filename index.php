bf19434c544adb94632ed7b323afcbd25fc29e8d
John Larimore <john.m.larimore@gmail.com>
2023-04-09 21:33:39 -0600

    Initial setup

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+<html>
+	<head>
+		<title>PHP & MySQL</title>
+	</head>
+	<body>
+		<ul>
+			<li><a href="ch2/">CH2</a></li>
+			<li><a href="ch3/">CH3</a></li>
+			<li><a href="ch4/">CH4</a></li>
+			<li><a href="ch5/">CH5</a></li>
+			<li><a href="ch6/">CH6</a></li>
+			<li><a href="ch7/">CH7</a></li>
+			<li><a href="ch8/">CH8</a></li>
+			<li><a href="ch9/">CH9</a></li>
+			<li><a href="ch10/">CH10</a></li>
+			<li><a href="ch11/">CH11</a></li>
+			<li><a href="ch12/">CH12</a></li>
+			<li><a href="ch13/">CH13</a></li>
+			<li><a href="ch14/">CH14</a></li>
+			<li><a href="ch15/">CH15</a></li>
+			<li><a href="ch16/">CH16</a></li>
+			<li><a href="ch17/">CH17</a></li>
+		</ul>
+	</body>
+</html>
