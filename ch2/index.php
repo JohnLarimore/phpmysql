1a98114292ddf84f562d1efd108038e36aa46473
John Larimore <john.m.larimore@gmail.com>
2023-04-13 09:23:55 -0600

    Added ch2 updates

--- ch2/index.php
+++ ch2/index.php
@@ -1,11 +1,37 @@
 <?php
+$name = 'Ivy'; 
+$greeting =  'Hello'; 
+if($name) {
+	$greeting = 'Welcome back, ' . $name; 
+}
+$product = 'Lollipop';
+$cost = 2;
+//$totals = []; 
+for($i=1; $i<=10; $i++){
+	$subtotal = $cost * $i;
+	$discount = ($subtotal / 1000) * ($i * 4);
+	$totals[$i] = $subtotal - $discount; 
+}
 
 ?>
-<html>
-	<head>
-		<title>CH2</title>
-	</head>
-	<body>
-		
-	</body>
-</html>
+<?php require 'includes/header.php'; ?>
+	<p><?= $greeting ?></p>
+	<h2><?= $product ?>Discounts</h2>
+	<table>
+		<tr>
+			<th>Packs</th>
+			<th>Price</th>
+		</tr>
+		<?php foreach($totals as $quantity => $price) { ?>
+		<tr>	
+			<td>
+				<?= $quantity ?>
+				pack<?= ($quantity === 1) ? '' : 's'; ?>
+			</td>
+			<td>
+				$<?= $price ?>
+			</td>
+		</tr>
+		<?php } ?>
+	</table>
+<?php include 'includes/footer.php'; ?>
