0e5f9405bf83d7cc1b186455934e729b0258f243
John Larimore <john.m.larimore@gmail.com>
2023-04-13 09:21:20 -0600

    Added ch1 index

--- ch1/index.php
+++ ch1/index.php
@@ -1,11 +1,27 @@
 <?php
-
+$username = 'Ivy'; 
+$greeting = 'Hello, ' . $username . '.'; 
+$offer = [
+	'item' => 'Chocolate',
+	'qty' => 5,
+	'price' => 5,
+	'discount' => 4,
+];
+$usual_price = $offer['qty'] * $offer['price']; 
+$offer_price = $offer['qty'] * $offer['discount']; 
+$saving = $usual_price - $offer_price; 
 ?>
 <html>
 	<head>
-		<title>CH1</title>
+		<title>The Candy Store</title>
+		<link rel="stylesheet" href="css/styles.css">
 	</head>
 	<body>
-		
+		<h1>The Candy Store</h1>
+		<h2>Multi-buy Offer</h2>
+		<p><?= $greeting ?></p>
+		<p class="sticker">Save $<?= $saving ?></p>
+		<p>Buy <?= $offer['qty'] ?> packs of <?= $offer['item'] ?>
+			for $<?= $offer_price ?> (usual price of $<?= $usual_price ?>)</p>
 	</body>
 </html>
